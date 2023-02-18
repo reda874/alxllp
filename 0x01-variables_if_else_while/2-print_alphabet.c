9b6df6e1b8530e55b37b39c470d11243c3df25ce
reda874 <falakireda1@gmail.com>
2023-02-18 02:32:40 +0100

    c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,14 +7,10 @@ int main(void)
 {
 	char lc;
 
-	for
-		lc = 'a';
-		lc <= 'z;
-		lc++;
-	{
-		putchar(lc);
+	for (lc = 'a'; lc <= 'z'; lc++);
+{
+	putchar(lc);
 	}
-
 	putchar('\n');
 	return (0);
 }
