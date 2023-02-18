de0a231437374cc03a01f95ff916ddcf3e6328d5
reda874 <falakireda1@gmail.com>
2023-02-18 03:01:32 +0100

    c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ *main - printing  digit numbers of base 10 starting
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	while (n < 10)
+	{
+		printf("%i", n);
+		n++;
+	}
+	putchar('\n');
+
+	return (0);
+}
