c226070a3e90b0dc88a0ca42b65c2cf31b21be43
reda874 <falakireda1@gmail.com>
2023-02-22 00:48:22 +0100

    fnl

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include"main.h"
+/**
+ * print alphabet x10 - printing 10 times alphabet
+ *
+ */
+void print_alphabet(void)
+{
+	char ch;
+	int i;
+
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+		}
+}
