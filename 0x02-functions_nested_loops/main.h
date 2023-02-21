b5356003a278c71b9cbf1a4d0d380516ee1650b1
reda874 <falakireda1@gmail.com>
2023-02-21 23:09:02 +0100

    fnl

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_h
+#define MAIN_H
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int _print_sign(int n);
+int _abs(int);
+int _print_last_digit(int num);
+void jack_bauer(void);
+int add(int num1, int num2);
+void print_to_98(int n);
+
+#endif
