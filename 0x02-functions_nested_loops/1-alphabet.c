1b4ecda5954a9b86695eb64b08001187e062de4c
reda874 <falakireda1@gmail.com>
2023-02-22 01:24:21 +0100

    fnl

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,24 +1,14 @@
-#include"main.h"
+#include "main.h"
 /**
- * print alphabet x10 - printing 10 times alphabet
+ * print_alphabet  - printing alphabet
  *
  */
-void print_alphabet_x10(void)
+
+void print_alphabet(void)
 {
-	char ch;
-	int i;
+	char letter;
 
-	i = 0;
-
-	while (i < 10)
-	{
-		ch = 'a';
-		while (ch <= 'z')
-		{
-			_putchar(ch);
-			ch++;
-		}
-		_putchar('\n');
-		i++;
-		}
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
 }
