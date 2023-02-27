4f4bfcbab08067950a88c466c4808b54b245928f
reda874 <falakireda1@gmail.com>
2023-02-27 14:12:56 +0100

    pointers arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include <main.h>
+/**
+ * reset_to_98 - changing value of an int with a pointer
+ * @n: input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+understanding pointers arrays and strings
