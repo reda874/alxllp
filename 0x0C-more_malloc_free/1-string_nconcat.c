d572520762057a72b12645a318cdf8bed9024498
reda874 <falakireda1@gmail.com>
2023-03-16 23:55:59 +0100

    more malloc free

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,45 +1,49 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
-* string_nconcat - concatenates two strings.
-* @s1: first char
-* @s2: second char
-* @n: unsigned int
-*
-* Return: NULLif error
-*/
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * @n: bytes
+ *
+ * Return: pointer to the allocated memory
+ * if fail, status 98
+ */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-   unsigned int x, y, z;char *s;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
+	if (s1 == NULL)
+		s1 = "";
 
-   if (s1 == NULL)
-   {
-       x = 0;
-   }
-   else
-   {
-       for (x = 0; s1[x]; ++x)
-           ;
-   }
-   if (s2 == NULL)
-   {
-       y = 0;
-   }
-   else
-   {
-       for (y = 0; s2[y]; ++y)
-           ;
-   }
-   if (y > n)
-       y = n;
-   s = malloc(sizeof(char) * (x + y + 1));
-   if (s == NULL)
-       return (NULL);
-   for (z = 0; z < x; z++)
-       s[z] = s1[z];
-   for (z = 0; z < y; z++)
-       s[z + x] = s2[z];
-   s[x + y] = '\0';
-   return (s);
+	if (s2 == NULL)
+		s2 = "";
+
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
+
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
--- 0x0C-more_malloc_free/_putchar.c
+++ 0x0C-more_malloc_free/_putchar.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <unistd.h>
+
 /**
  * _putchar - write the character c to stdout
  * @c: the character to print
